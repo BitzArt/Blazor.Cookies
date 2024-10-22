7a357cdac98a99b30eab2badadf695f3a48c1b18
Martin Vlček <84700611+VlcekM@users.noreply.github.com>
2024-10-23 00:22:27 +0200

    Adds the option to register the ICookieService as a singleton in WASM apps.

--- src/BitzArt.Blazor.Cookies.Client/Extensions/AddBlazorCookiesExtension.cs
+++ src/BitzArt.Blazor.Cookies.Client/Extensions/AddBlazorCookiesExtension.cs
@@ -10,4 +10,10 @@ public static class AddBlazorCookiesExtension
         builder.Services.AddScoped<ICookieService, BrowserCookieService>();
         return builder;
     }
+
+    public static WebAssemblyHostBuilder AddBlazorCookiesSingleton(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddSingleton<ICookieService, BrowserCookieService>();
+        return builder;
+    }
 }
