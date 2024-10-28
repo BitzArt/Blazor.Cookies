af76c63e83ad23b35af9db615660cfe2a88ae11d
Yuriy Durov <u.a.durov@gmail.com>
2024-10-28 17:47:38 +0400

    Cleanup

--- src/BitzArt.Blazor.Cookies.Server/Extensions/AddBlazorCookiesExtension.cs
+++ src/BitzArt.Blazor.Cookies.Server/Extensions/AddBlazorCookiesExtension.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
--- src/BitzArt.Blazor.Cookies.Server/Services/HttpContextCookieService.cs
+++ src/BitzArt.Blazor.Cookies.Server/Services/HttpContextCookieService.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics;
 
 namespace BitzArt.Blazor.Cookies;
 
